89b8c8694464b9c243cb1c8d12aacca7d4297d5d
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-01-14 14:52:52 +0100

    Created TopContent component, also TopContentText, which is representing the text of the TopContent

--- src/App.js
+++ src/App.js
@@ -3,21 +3,22 @@ import "../public/files/images/Logo.svg";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getPositions, getUsers, showmessage, showMessage, signUp } from "./redux/thunks";
+import TopContent from "./components/Top_content/Top_Content";
 
 function App(){
     const dispatch = useDispatch();
 
     useEffect(() =>{
-        dispatch(getPositions());
-        dispatch(getUsers({ offset: 0, count: 5 }));
+        //dispatch(getPositions());
+        //dispatch(getUsers({ offset: 0, count: 5 }));
     }, [])
 
-    const message = useSelector((state) => state.message);
+
+
 
     return(
         <div>
-            <h2>Working</h2>
-            {message ? "TObi gg" : "Harosh"} 
+            <TopContent />
         </div>
     );
 }
---
+++ src/components/Top_content/Top_Content.js
@@ -0,0 +1,18 @@
+const title = "Test assignment for front-end developer";
+const text = 
+<span>
+    What defines a good frontend developer is one that has skilled knowledge of HTML, CSS, JS with a vast understanding of User design thinking as they'll be building web interfaces with accessibility n mind. 
+    <br/>They should also be excited to learn as the world of Front-End Deveopment keeps evolving.
+</span>
+
+import TopContentText from "./Top_content_text";
+
+function TopContent(){
+    return(
+        <div className="topContent">
+            <TopContentText title={title} text={text} />
+        </div>
+    );
+}
+
+export default TopContent;
