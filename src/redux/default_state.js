f0193f6ea525d8f8c66028bf65016f85d06dd568
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-01-14 12:44:47 +0100

    Added case for handling positions fetching

--- src/redux/default_state.js
+++ src/redux/default_state.js
@@ -1,3 +1,7 @@
 export const defaultState = {
-    users: {}
+    users: {
+        users: [],
+        usersData: null,
+        positions: []
+    }
 }
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,4 +1,4 @@
-import { GET_USERS } from "./action_types";
+import { GET_POSITIONS, GET_USERS } from "./action_types";
 import { defaultState } from "./default_state";
 
 export function users(state = defaultState.users, action){
@@ -6,6 +6,9 @@ export function users(state = defaultState.users, action){
         case GET_USERS: {
             console.log("Getting the users")
             return state;
+        }
+        case GET_POSITIONS: {
+            return {...state, positions: action.payload.positions };
         }
         default:{
             return state;
