38acafbe7131646b32e448544cb308fb916a8f14
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-01-14 20:14:02 +0100

    Created empty WorkingWithPostRequest component

--- src/components/Main_page/Main_page.js
+++ src/components/Main_page/Main_page.js
@@ -1,11 +1,13 @@
 import TopContent from "../Top_content/Top_Content";
 import WorkingWithGetRequest from "../Working_with_get_request/Working_with_get_request";
+import WorkingWithPostRequest from "../Working_with_post_request/Working_with_post_request";
 
 function MainPage(){
     return(
         <div className="mainPage">
             <TopContent />
             <WorkingWithGetRequest />
+            <WorkingWithPostRequest />
         </div>
     );
 }
---
+++ src/components/Working_with_post_request/Working_with_post_request.js
@@ -0,0 +1,12 @@
+function WorkingWithPostRequest(){
+    return(
+        <div className="workingWithPostRequest">
+            <h2>Working with post request</h2>
+            <form className="signUpForm" method="POST">
+
+            </form>
+        </div>  
+    );
+}
+
+export default WorkingWithPostRequest;
