b896ab2caa7ac1fff31e4099c38a6c1b70884416
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-01-14 10:57:43 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./css/main.scss";
+import "../public/files/images/Logo.svg";
+
+function App(){
+    return(
+        <div>
+            <h2>Working</h2>
+        </div>
+    );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "bundle"),
+        filename: "bundle.js",
+    },
+
+    module:{
+        rules:[
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: { loader: "babel-loader" }
+            },
+
+            {
+                test: /\.(css|scss)$/,
+                use: [ "style-loader", "css-loader", "sass-loader" ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i, 
+                loader: 'file-loader',
+                options: {
+                  name: '/public/files/images/[name].[ext]'
+                }
+            },
+        ]
+    },
+
+    plugins: [ new HtmlWebpackPlugin({ template: "./src/index.html" }) ]
+}
