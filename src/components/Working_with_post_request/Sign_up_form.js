1e79455fd6f85a0508d09ef325b6da442bdf0c27
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-01-14 20:31:48 +0100

    Added error showing

--- src/components/Working_with_post_request/Sign_up_form.js
+++ src/components/Working_with_post_request/Sign_up_form.js
@@ -3,7 +3,7 @@ import { useForm } from "react-hook-form";
 
 function SignUpForm(){
     const { formState, register, handleSubmit } = useForm({
-        mode: "onSubmit"
+        mode: "onChange"
     });
 
     return(
@@ -16,9 +16,14 @@ function SignUpForm(){
                         required: "Name is required", minLength: { value: 2, message: "Name length must be greater than 2" }, maxLength: { value: 100, message: "Name length must be less than 100" } }
                 )} 
             />
-            
+            <span>{ formState.errors.name?.message }</span>
+
             <Input type="email" placeholder="Email" />
+            <span>{ formState.errors.email?.message }</span>
+
             <Input type="tel" placeholder="Phone" />
+            <span>{ formState.errors.phone?.message }</span>
+
 
             <Button type="submit" variant="contained">Sing up</Button>
         </form>
