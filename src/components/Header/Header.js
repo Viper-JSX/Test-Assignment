8020b57426241a73fd425047a60ae9da149194a5
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-01-15 19:23:17 +0100

    Aligned header buttons and icon, added styling to 'goToSignUp' and 'goToShowMoreUsers' buttons

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,9 +4,11 @@ import { Button } from "@mui/material";
 function Header(){
     return(
         <header>
-            <img src="../../../public/files/images/Logo.svg" />
-            <Button href="#" variant="contained">Users</Button>
-            <Button href="#" variant="contained">Users</Button>
+            <img className="appIcon" src="../../../public/files/images/Logo.svg" />
+            <div className="headerButtons">
+                <Button className="goToSignUp" href="#" variant="contained">Sing up</Button>
+                <Button className="goToShowMoreUsers" href="#" variant="contained">Users</Button>
+            </div>
         </header>
     )
 }
