23ba94978a351e647cd9572033a78cde14b9272b
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-01-15 20:46:03 +0100

    Implemented user's long name, occupation and email hiding, added 'goToSignUp' button to top content

--- src/components/Working_with_get_request/Users.js
+++ src/components/Working_with_get_request/Users.js
@@ -2,7 +2,45 @@ import { useSelector } from "react-redux";
 import UserCard from "./User_card";
 
 function Users(){
-    const users = useSelector((state) => state.users.users);
+    //const users = useSelector((state) => state.users.users);
+    const users = [
+        {
+            name: "Van",
+            position: "Musor",
+            email: "psina@hmao.com",
+            phone: "1244567889",
+            photo: "none"
+        },
+        {
+            name: "Van",
+            position: "Musorpsina@hmao.compsina@hmao.com psina@hmao.com psina@hmao.compsina@hmao.compsina@hmao.compsina@hmao.compsina@hmao.compsina@hmao.compsina@hmao.com",
+            email: "psina@hmao.psina@hmao.psina@hmao.psina@hmao.psina@hmao.psina@hmao.psina@hmao.psina@hmao.psina@hmao.psina@hmao.",
+            phone: "1244567889",
+            photo: "none"
+        },
+        {
+            name: "Van",
+            position: "Musor",
+            email: "psina@hmao.com",
+            phone: "1244567889",
+            photo: "none"
+        },
+        {
+            name: "Van",
+            position: "Musor",
+            email: "psina@hmao.com",
+            phone: "1244567889",
+            photo: "none"
+        },
+        {
+            name: "Van",
+            position: "Musor",
+            email: "psina@hmao.com",
+            phone: "1244567889",
+            photo: "none"
+        }
+    ];
+
     console.log(users)
 
     return(
