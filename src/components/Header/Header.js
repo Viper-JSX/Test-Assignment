f65b464703ecc55501c5a7097edd3e864325aa53
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-01-15 19:36:20 +0100

    Header's items wrapped by a container to align them right

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,10 +4,12 @@ import { Button } from "@mui/material";
 function Header(){
     return(
         <header>
-            <img className="appIcon" src="../../../public/files/images/Logo.svg" />
-            <div className="headerButtons">
-                <Button className="goToSignUp" href="#" variant="contained">Sing up</Button>
-                <Button className="goToShowMoreUsers" href="#" variant="contained">Users</Button>
+            <div className="headerItemsContainer">
+                <img className="appIcon" src="../../../public/files/images/Logo.svg" />
+                <div className="headerButtons">
+                    <Button className="goToSignUp" href="#" variant="contained">Sing up</Button>
+                    <Button className="goToShowMoreUsers" href="#" variant="contained">Users</Button>
+                </div>
             </div>
         </header>
     )
