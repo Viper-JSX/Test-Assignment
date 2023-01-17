4509d49417e7a8a336a6d07aa608f7b59ba19ad2
Yurii Shtefanko <yuramcpecs@gmail.com>
2023-01-17 14:49:47 +0100

    Removed default users list

--- src/components/Working_with_get_request/Users.js
+++ src/components/Working_with_get_request/Users.js
@@ -3,44 +3,6 @@ import UserCard from "./User_card";
 
 function Users(){
     const users = useSelector((state) => state.users.users);
-    /*const users = [
-        {
-            name: "Van",
-            position: "Musor",
-            email: "psina@hmao.com",
-            phone: "1244567889",
-            photo: "none"
-        },
-        {
-            name: "Van",
-            position: "Musorpsina@hmao.compsina@hmao.com psina@hmao.com psina@hmao.compsina@hmao.compsina@hmao.compsina@hmao.compsina@hmao.compsina@hmao.compsina@hmao.com",
-            email: "psina@hmao.psina@hmao.psina@hmao.psina@hmao.psina@hmao.psina@hmao.psina@hmao.psina@hmao.psina@hmao.psina@hmao.",
-            phone: "1244567889",
-            photo: "none"
-        },
-        {
-            name: "Van",
-            position: "Musor",
-            email: "psina@hmao.com",
-            phone: "1244567889",
-            photo: "none"
-        },
-        {
-            name: "Van",
-            position: "Musor",
-            email: "psina@hmao.com",
-            phone: "1244567889",
-            photo: "none"
-        },
-        {
-            name: "Van",
-            position: "Musor",
-            email: "psina@hmao.com",
-            phone: "1244567889",
-            photo: "none"
-        }
-    ];
-    */
 
     return(
         <div className="users">
